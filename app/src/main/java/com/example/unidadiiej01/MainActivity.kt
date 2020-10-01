43e00cd98f5052ac578d3f2a8a99bdea56915931
Luis Cobián <coby10@gmail.com>
2020-10-01 13:54:14 -0500

    Cambio con el boton para agregar numeros

--- app/src/main/java/com/example/unidadiiej01/MainActivity.kt
+++ app/src/main/java/com/example/unidadiiej01/MainActivity.kt
@@ -6,18 +6,23 @@ import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+        var contador:Int = 0
         override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState) // cumplir herencia
         setContentView(R.layout.activity_main)  //
 
+
         // bind
         val nameTxt = findViewById<TextView>(R.id.txtName)
         val nameBtn = findViewById<Button>(R.id.btnName)
+        val countTxt = findViewById<TextView>(R.id.txtCount)
 
-        nameTxt.setText("Pendiente ......")
+        nameTxt.setText("Luis Cobián")
         
         nameBtn.setOnClickListener{
-            nameTxt.setText("Luis Cobian")
+            contador = contador.inc()
+            countTxt.setText("$contador")
         }
 
     }
